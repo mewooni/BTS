ba3de8593a5288b69d8b32977fca3f66206899d2
bcloud <100233498+mewooni@users.noreply.github.com>
2023-05-19 19:11:43 +0300

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let radio_buttons = document.querySelectorAll('.type_rb');
+let sections = document.querySelectorAll('.album_section');
+
+radio_buttons.forEach(rb => {
+    rb.addEventListener('change',function (){
+        sections.forEach(section =>{
+            if (rb.value === section.id){
+                section.classList.remove('hidden');
+            }
+            else{
+                section.classList.add('hidden');
+            }
+        });
+    });
+});
